6f15ecd919c904b029da7d9046b3d585125069de
Александр Гулеватый <aleksandrgulevatyj@MacBook-Air-Aleksandr.local>
2023-03-27 09:57:54 +0800

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,36 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: 'salary - 600$ month',
+  address: 'м. Хмельницький вул. Героїв Крут',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'ivanov@mail.com',
+      href: 'mailto:ivanov@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+  address: 'м. Хмельницький, вул. Героїв крут',
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -23,6 +53,242 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Summury',
+    },
+
+    header,
+
+    main: {
+      summury: {
+        title: 'Summury',
+        text: `Open-minded for new technologies, with 1 years of
+        experience in development. Whenever I start to
+        work on a new project I learn the domain and try
+        to understand the idea of the project. Good team
+        player, every colleague is a friend to me.`,
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parcing sport betting data from different platforms
+        (odds) and sport statistic (tournament position, job etc.) analyzing bysimple
+        mathematics models and preparingprobility for such events like :
+        money line - first win / draw / second win, total etc.`,
+      },
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 9,
+          isTop: false,
+        },
+        {
+          name: 'VS Code',
+          point: 8,
+          isTop: false,
+        },
+        {
+          name: 'NPM',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Git',
+          point: 6,
+          isTop: false,
+        },
+        {
+          name: 'Terminal',
+          point: 5,
+          isTop: false,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+      hobbies: [
+        {
+          name: 'Dancing',
+          isMain: false,
+        },
+        {
+          name: 'Singing',
+          isMain: false,
+        },
+        {
+          name: 'Playing games',
+          isMain: true,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+
+    main: {
+      education: [
+        {
+          name: 'TBL#4',
+          isEnd: true,
+        },
+        {
+          name: 'KHNU',
+          isEnd: true,
+        },
+        {
+          name: 'Univercity of Ivan Franko',
+          isEnd: false,
+        },
+        {
+          name: 'Univercity of technology',
+          isEnd: true,
+        },
+        {
+          name: 'MAUP',
+          isEnd: false,
+        },
+      ],
+      certificates: [
+        {
+          name: 'Bachalor',
+          id: 321,
+        },
+        {
+          name: 'Master',
+          id: 123,
+        },
+        {
+          name: 'Teacher',
+          id: 456,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'http://it-brains.com.ua/',
+          },
+          duration: {
+            from: '10.10.2020',
+            to: null,
+          },
+          projectamount: 3,
+
+          projects: [
+            {
+              name: 'Resume',
+              url: 'http://resume.com.ua/',
+              about:
+                'I did a lot of stuff with this project, you can check my GitHub',
+              project: [
+                {
+                  name: 'Project stacks',
+                },
+              ],
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Nodejs',
+                },
+              ],
+              award: [
+                {
+                  name: 'Project awards',
+                },
+              ],
+              awards: [
+                {
+                  name: 'One of the best Dark Souls gamer ever',
+                },
+                {
+                  name: 'It was really wonderful',
+                },
+              ],
+              stackamount: 3,
+              awardamount: 2,
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
   })
 })
 
